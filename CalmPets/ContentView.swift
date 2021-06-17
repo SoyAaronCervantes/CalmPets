59d35ee76f668663a89bbd68e9c6c98017eaedab
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2021-06-16 20:19:09 -0500

    Initial Commit

---
+++ CalmPets/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CalmPets
+//
+//  Created by Aarón Cervantes Álvarez on 16/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
