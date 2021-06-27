7982d2fb52e84ef7e6125941d6f0aa7e562fa52b
Aarón Cervantes <soy.aaron.cervantes@gmail.com>
2021-06-27 03:07:53 -0500

    Added pickers to PetFormView

--- CalmPets/Views/Navigation/PetFormView.swift
+++ CalmPets/Views/Navigation/PetFormView.swift
@@ -13,38 +13,76 @@ struct PetFormView: View {
     @Binding var offset: CGFloat
     @Binding var pet: PetModel
     
-    private let store = Firestore.firestore()
+    @State var name = ""
+    @State var age = ""
+    @State var selectedAnimal: AnimalsEnum = .perro
+    @State var selectedGenre: GenresEnum = .masculino
+    
+    let store = Firestore.firestore()
+
+    var genres = ["Masculino", "Femenino"]
 
     var body: some View {
-        VStack {
-            HStack(spacing: 15) {
-                Button(action: {
-                    offset = CGFloat(0)
-                }, label: {
-                    HStack {
-                        Image(systemName: "chevron.left")
-                            .font(.system(size: 24, weight: .bold))
-                        Text("Mascotas")
-                            .font(.title2)
-                            .fontWeight(.bold)
-                    }
-                })
-                Spacer()
-            }
-            .foregroundColor(.primary)
-            .padding()
-            
-            ScrollView {
+        NavigationView {
+            VStack {
                 
+                HStack(spacing: 15) {
+                    Button(action: {
+                        offset = CGFloat(0)
+                    }, label: {
+                        HStack {
+                            Image(systemName: "chevron.left")
+                                .font(.system(size: 24, weight: .bold))
+                            Text("Mascotas")
+                                .font(.title2)
+                                .fontWeight(.bold)
+                        }
+                    })
+                    
+                    Spacer()
+                }
+                .foregroundColor(.primary)
+                .padding()
+
+                Form {
+                    Section( header: Text("Datos de la mascota") ) {
+
+                        TextField("Nombre", text: $name)
+                        TextField("Edad", text: $age)
+                            .keyboardType(.numberPad)
+
+                        Picker(selection: $selectedAnimal, label: Text("¿Qué animal es?")) {
+                            ForEach(AnimalsEnum.allCases, id: \.self) { animal in
+                                Text( animal.name ).tag(animal)
+                            }
+                        }
+                        
+                        Picker(selection: $selectedGenre, label: Text("¿Qué genero tiene?")) {
+                            ForEach(GenresEnum.allCases, id: \.self) { genre in
+                                Text( genre.name ).tag(genre)
+                            }
+                        }
+
+                    }
+                }
             }
+            .frame(maxWidth: .infinity)
+            .padding(.bottom, edges?.bottom ?? 15)
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarBackButtonHidden(true)
+            .navigationBarHidden(true)
         }
-        .frame(maxWidth: .infinity)
-        .padding(.top, edges?.top ?? 15)
-        .padding(.bottom, edges?.bottom ?? 15)
+        
     }
     
     private func endEditing() {
         UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to:nil, from:nil, for:nil)
     }
     
+}
+
+extension View {
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
 }
